8c35577c467d5a01e96a2abd76b274041ad90020
SEYED MOHAMMAD SEYED MOTEHAYERI <motehayeri@ut.ac.ir>
2025-02-04 21:30:36 +0330

    Load Main Scene from Main Menu
    Write a method to load the Main scene

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -17,6 +18,7 @@ public class MenuUIHandler : MonoBehaviour
     
     private void Start()
     {
+        SceneManager.LoadScene(1);
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
