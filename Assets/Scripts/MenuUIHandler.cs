7fc6cd7767a0c3df13ef2d99d03bbec0d6c3d58b
SEYED MOHAMMAD SEYED MOTEHAYERI <motehayeri@ut.ac.ir>
2025-02-04 22:21:10 +0330

    UI back to menu
    Set up the transition back to the Menu scene

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -29,6 +30,10 @@ public class MenuUIHandler : MonoBehaviour
     }
     public void Exit()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
 }
