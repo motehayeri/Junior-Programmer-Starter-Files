5f7de33c32fc8598f859ada556470da6810a914d
SEYED MOHAMMAD SEYED MOTEHAYERI <motehayeri@ut.ac.ir>
2025-02-06 02:03:49 +0330

    Update ProductivityUnit.cs

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -1,15 +1,8 @@
 using UnityEngine;
 
-public class ProductivityUnit : MonoBehaviour
+public class ProductivityUnit : Unit
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    protected override void BuildingInRange()
     {
         
     }
